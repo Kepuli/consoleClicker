e0fa81555a2f784ef6eb4f8dae5dd9b7c524e2e3
Kepuli <tuomas.talasmaa@gmail.com>
2015-04-10 08:44:56 +0300

    modified:   ConsoleClikker/ConsoleClikker.csproj 	modified:   ConsoleClikker/Program.cs 	new file:   ConsoleClikker/Tilanne.cs 	modified:   ConsoleClikker/bin/Debug/ConsoleClikker.exe 	modified:   ConsoleClikker/bin/Debug/ConsoleClikker.pdb 	modified:   ConsoleClikker/obj/Debug/ConsoleClikker.csproj.FileListAbsolute.txt 	modified:   ConsoleClikker/obj/Debug/ConsoleClikker.csprojResolveAssemblyReference.cache 	modified:   ConsoleClikker/obj/Debug/ConsoleClikker.exe modified:   ConsoleClikker/obj/Debug/ConsoleClikker.pdb 	modified:   ConsoleClikker/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- ConsoleClikker/Program.cs
+++ ConsoleClikker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleClikker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace ConsoleApplication3 {
         static void Main(string[] args) {
 
             Console.WriteLine("COOKIE CLIKKER");
-            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
             Console.WriteLine("grandmama maksaa 100");
             Console.WriteLine("farmi maksaa 500");
             Console.WriteLine("minor upgrade maksaa 20.000");
@@ -23,111 +24,107 @@ namespace ConsoleApplication3 {
             Console.WriteLine();
             Console.WriteLine("NEW UPDATE test now!");
             Console.WriteLine();
-            int z2 = 5;
-            int x2 = 200;
-            int z = 5;
-            int x = 0;
-            int y = 0;
-            int korotus = 1;
-            bool butterKaytetty = false;
+
+
+            Tilanne tilanne = new Tilanne();
             do {
                 string kirj = Console.ReadLine();
                 if (kirj == "grandmama") {
-                    if (x >= 100) {
+                    if (tilanne.x >= 100) {
                         Console.WriteLine("sinulla on nyt yksi uusi grandmama");
-                        korotus++;
-                        x = x - 100;
-                        y = y + 1;
+                        tilanne.korotus++;
+                        tilanne.x = tilanne.x - 100;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "farm") {
-                    if (x >= 500) {
+                    if (tilanne.x >= 500) {
                         Console.WriteLine("sinulla on nyt yksi uusi farmi");
-                        korotus = korotus + 6;
-                        x = x - 500;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + 6;
+                        tilanne.x = tilanne.x - 500;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "factory") {
-                    if (x >= 50000) {
+                    if (tilanne.x >= 50000) {
                         Console.WriteLine("sinulla on uusi factory!");
-                        korotus = korotus + 50;
-                        x = x - 100000;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + 50;
+                        tilanne.x = tilanne.x - 100000;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "redmatter condenser") {
-                    if (x >= 700000) {
+                    if (tilanne.x >= 700000) {
                         Console.WriteLine("sinulla on uusi redmatter condenser!");
-                        korotus = korotus + x2;
-                        x = x - 700000;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + tilanne.x2;
+                        tilanne.x = tilanne.x - 700000;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "darkmatter condenser") {
-                    if (x >= 10000000) {
+                    if (tilanne.x >= 10000000) {
                         Console.WriteLine("sinulla on uusi Darkmatter condenser!");
-                        korotus = korotus + 900;
-                        x = x - 10000000;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + 900;
+                        tilanne.x = tilanne.x - 10000000;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "mother cookie") {
-                    if (x >= 50000000) {
+                    if (tilanne.x >= 50000000) {
                         Console.WriteLine("sinulla on uusi mother cookie!");
-                        korotus = korotus + 6000;
-                        x = x - 5000000;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + 6000;
+                        tilanne.x = tilanne.x - 5000000;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
 
                 if (kirj == "god") {
-                    if (x >= 100000000) {
+                    if (tilanne.x >= 100000000) {
                         Console.WriteLine("sinulla on COOKIE GOD!");
-                        korotus = korotus + 15000;
-                        x = x - 100000000;
-                        y = y + 1;
+                        tilanne.korotus = tilanne.korotus + 15000;
+                        tilanne.x = tilanne.x - 100000000;
+                        tilanne.y = tilanne.y + 1;
                     } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä");
                 }
                 if (kirj == "minor upgrade") {
-                    if (x >= 20000) {
-                        if (z <= 10) {
+                    if (tilanne.x >= 20000) {
+                        if (tilanne.z <= 10) {
                             Console.WriteLine("sinun ostokset tuottavat nyt 1 enemmän kuin ennen");
-                            korotus = korotus + y;
-                            x = x - 20000;
-                            z = z + 10;
+                            tilanne.korotus = tilanne.korotus + tilanne.y;
+                            tilanne.x = tilanne.x - 20000;
+                            tilanne.z = tilanne.z + 10;
                         } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä tai olet jo ostanut tämän päivityksen");
                     }
                 }
                 if (kirj == "major upgrade") {
-                    if (x >= 200000) {
-                        if (z <= 20) {
+                    if (tilanne.x >= 200000) {
+                        if (tilanne.z <= 20) {
                             Console.WriteLine("sinun ostokset tuottavat nyt 8 enemmän kuin ennen");
-                            korotus = korotus + 8 * y;
-                            x = x - 200000;
-                            z = z + 10;
+                            tilanne.korotus = tilanne.korotus + 8 * tilanne.y;
+                            tilanne.x = tilanne.x - 200000;
+                            tilanne.z = tilanne.z + 10;
                         } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä tai olet jo ostanut tämän päivityksen");
                     }
                 }
                 if (kirj == "the red redmatter") {
-                    if (x >= 30000000) {
-                        if (z2 <= 10) {
+                    if (tilanne.x >= 30000000) {
+                        if (tilanne.z2 <= 10) {
                             Console.WriteLine("tästä eteenpäin jokainen redmatter condenser tuottaa 100 enemmäm");
-                            x2 = x2 + 100;
-                            x = x - 30000000;
-                            z2 = z2 + 10;
+                            tilanne.x2 = tilanne.x2 + 100;
+                            tilanne.x = tilanne.x - 30000000;
+                            tilanne.z2 = tilanne.z2 + 10;
                         } else Console.WriteLine("sinulla ei ole tarpeeksi keksejä tai olet jo ostanut tämän päivityksen");
                     }
                 }
                 if (kirj == "butter is better") {
-                    if (x >= 600000000) {
-                        if (butterKaytetty)
+                    if (tilanne.x >= 600000000) {
+                        if (tilanne.butterKaytetty)
                             Console.WriteLine("sinulla ei ole tarpeeksi keksejä tai olet jo ostanut tämän päivityksen");
                         else {
                             Console.WriteLine("sinun tämän hetkinen per click on tuplaantunut");
-                            korotus = korotus + korotus;
-                            x = x - 600000000;
-                            butterKaytetty = true;
+                            tilanne.korotus = tilanne.korotus + tilanne.korotus;
+                            tilanne.x = tilanne.x - 600000000;
+                            tilanne.butterKaytetty = true;
                         }
                     }
                 }
@@ -145,19 +142,13 @@ namespace ConsoleApplication3 {
                     Console.WriteLine("COOKIE GOD maksaa 100.000.000");
                 }
                 if (kirj == "/give 100 000 000 cookies") {
-                    x = x + 100000000;
+                    tilanne.x = tilanne.x + 100000000;
                 }
                 if (kirj == "/restart") {
-                    x = 0;
-                    z = 0;
-                    y = 0;
-                    x2 = 200;
-                    z2 = 5;
-                    butterKaytetty = false;
-                    korotus = 1;
+                    tilanne.Restart();
                 }
-                x = x + korotus;
-                Console.WriteLine("cookie " + x.ToString());
+                tilanne.x = tilanne.x + tilanne.korotus;
+                Console.WriteLine("cookie " + tilanne.x.ToString());
             } while (true);
 
 
---
+++ ConsoleClikker/Tilanne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClikker {
+    public class Tilanne {
+        public int z2;
+        public int x2;
+        public int z; 
+        public int x;
+        public int y;
+        public int korotus;
+        public bool butterKaytetty;
+
+        public Tilanne() {
+            Restart();
+        }
+
+        public bool GrandmamaOK() {
+            return x >= 100;
+        }
+
+        internal void Restart() {
+            x = 0;
+            z = 0;
+            y = 0;
+            x2 = 200;
+            z2 = 5;
+            butterKaytetty = false;
+            korotus = 1;
+        }
+    }
+}
